ac7357f7be0ffe6f1884bb5c1480c1331304649f
Khai Yi Chin <khaiyichin@gmail.com>
2024-04-10 15:53:32 -0400

    Added static degradation simulation files.

---
+++ scripts/python/run_static_degradation_experiment.py
@@ -0,0 +1,119 @@
+import sim_module as sm
+import json
+import yaml
+import argparse
+# from joblib import Parallel, delayed
+from datetime import datetime
+import timeit
+import os
+import sys
+
+def parse_yaml_param_file(yaml_filepath):
+    """Parse the YAML parameter file.
+    """
+
+    with open(yaml_filepath) as fopen:
+        try:
+            config = yaml.safe_load(fopen)
+        except yaml.YAMLError as exception:
+            print(exception)
+    
+        # Displayed processed arguments
+        print("\n\t" + "="*15 + " Processed Parameters " + "="*15 + "\n")
+        print("\t\t", end="")
+        for line in yaml.dump(config, indent=4, default_flow_style=False):        
+            if line == "\n":
+                print(line, "\t\t",  end="")
+            else:
+                print(line, end="")
+        print("\r", end="") # reset the cursor for print
+        print("\n\t" + "="*13 + " End Processed Parameters " + "="*13  + "\n")
+
+    param_obj = sm.SimParam(config)
+
+    return param_obj
+
+def create_data_folder():
+    """Create a folder named data and use it as the working directory.
+    """
+
+    # Create the `data` folder if it doesn't exist
+    try:
+        os.mkdir("data")
+        os.chdir("data")
+        
+    except FileExistsError:
+
+        # Change to `data` directory
+        try:
+            os.chdir("data")
+        except Exception:
+            pass
+
+def create_simulation_folder(suffix, curr_time):
+    """Create a simulation folder within the `data` directory.
+    """
+
+    # Create data folder and change working directory
+    create_data_folder()
+
+    # Define folder name
+    folder_name = curr_time + suffix
+
+    # Create folder to store simulation data based on current datetime if doesn't exist (shouldn't exist, really)
+    try:
+        os.mkdir(folder_name)
+        os.chdir(folder_name)
+
+    except FileExistsError:
+        print("folder {} already exists".format(folder_name))
+
+        try:
+            os.chdir(folder_name)
+        except Exception:
+            pass
+
+def main():
+    parser = argparse.ArgumentParser(description="Execute multi-agent simulation with static topologies and static degradation.")
+    parser.add_argument("FILE", type=str, help="path to the parameter file relative to the current/execution directory")
+    parser.add_argument("-p", action="store_true", help="flag to use cores to run simulations in parallel")
+    args = parser.parse_args()
+
+    # Obtain timestamp
+    curr_time = datetime.now().strftime("%m%d%y_%H%M%S")
+
+    # Parse simulation parameters
+    param_obj = parse_yaml_param_file(args.FILE)
+
+    # Create a folder to store simulation data
+    create_simulation_folder(param_obj.suffix, curr_time)
+
+    if args.p:
+        pass
+    else:
+
+        for num_flawed_robots in param_obj.flawed_robot_range:
+            print("\nRunning case with # flawed robots = " + str(num_flawed_robots))
+
+            for trial in range(param_obj.num_trials):
+                start = timeit.default_timer()
+                print("\tRunning trial number = " + str(trial) + "... ", end="")
+                sys.stdout.flush()
+
+                s = sm.MultiRobotSimStaticDegradation(param_obj, trial, num_flawed_robots)
+
+                s.run()
+
+                # Write data to JSON file
+                s.save_data()
+
+                print("Done!\n")
+
+                end = timeit.default_timer()
+
+                print("\t-- Elapsed time:", end-start, "--\n")
+
+if __name__ == "__main__":
+    
+    main()
+    print("success")
---
+++ scripts/python/sim_module.py
@@ -0,0 +1,207 @@
+import notebooks.single_robot_sim_module as srsm
+
+import numpy as np
+from scipy import stats
+import json
+
+class SimParam:
+
+
+    def __init__(self, yaml_config):
+
+        # Common parameters
+        self.num_robots = int(yaml_config["numRobots"])
+        self.comms_period = int(yaml_config["commsPeriod"])
+        self.flawed_b = float(yaml_config["flawedSensorAccB"])
+        self.flawed_w = float(yaml_config["flawedSensorAccW"])
+        self.correct_b = float(yaml_config["correctSensorAccB"])
+        self.correct_w = float(yaml_config["correctSensorAccW"])
+        self.sensor_cov = float(yaml_config["flawedSensorCovariance"])
+        self.tfr = float(yaml_config["targFillRatio"])
+        self.sensor_filter_period = int(yaml_config["sensorFilterPeriod"])
+        self.num_steps = int(yaml_config["numSteps"])
+        self.num_trials = int(yaml_config["numTrials"])
+        self.fully_connected = bool(yaml_config["fullyConnected"])
+        self.method = str(yaml_config["method"])
+
+        # Flawed robot range
+        flawed_robots_min = int(yaml_config["flawedRobotRange"]["min"])
+        flawed_robots_max = int(yaml_config["flawedRobotRange"]["max"])
+        flawed_robots_steps = int(yaml_config["flawedRobotRange"]["incSteps"])
+        self.flawed_robot_range = np.arange(
+            flawed_robots_min,
+            flawed_robots_max+1,
+            flawed_robots_steps,
+            dtype=np.int32
+        ).tolist()
+
+        self.create_folder_suffix()
+
+    def create_folder_suffix(self):
+        """Create suffix for folder name.
+        """
+
+        if len(self.flawed_robot_range) == 1:
+            flawed_robot_inc = 0            
+        else:
+            flawed_robot_inc = self.flawed_robot_range[1] - self.flawed_robot_range[0]
+
+        min_num_flawed_robots, max_num_flawed_robots = int(self.flawed_robot_range[0]), int(self.flawed_robot_range[-1])
+        flawed_b, flawed_w = int(self.flawed_b * 1.0e3), int(self.flawed_w * 1.0e3)
+        correct_b, correct_w = int(self.correct_b * 1.0e3), int(self.correct_w * 1.0e3)
+
+        flawed_robot_suffix = "_flw" + str(min_num_flawed_robots) + "-" + str(flawed_robot_inc) + "-" + str(max_num_flawed_robots)
+        flawed_sensor_acc_suffix = "_flwb" + str(flawed_b) + "_flww" + str(flawed_w)
+        correct_sensor_acc_suffix = "_corb" + str(correct_b) + "_corw" + str(correct_w)
+        comms_period_suffix = "_commsp" + str(self.comms_period)
+        sensor_filter_period_suffix = "_filtp" + str(self.sensor_filter_period)
+
+        self.suffix = "_t" + str(self.num_trials) + "_s" + str(self.num_steps) + "_tfr" + str(int(self.tfr * 1.0e3)) + \
+            flawed_robot_suffix + flawed_sensor_acc_suffix + correct_sensor_acc_suffix + comms_period_suffix + sensor_filter_period_suffix
+
+class MultiRobotSimStaticDegradation:
+
+    def __init__(self, param: SimParam, trial_ind: int, num_flawed_robots: int):
+        self.env = srsm.Environment(param.tfr)
+        self.num_robots = param.num_robots
+        self.num_correct_robots = self.num_robots - num_flawed_robots
+        self.init_b = param.flawed_b
+        self.init_w = param.flawed_w
+        self.sensor_cov = param.sensor_cov
+        self.act_b = param.correct_b
+        self.act_w = param.correct_w
+        self.tfr = param.tfr
+        self.sensor_filter_period = param.sensor_filter_period
+        self.comms_period = param.comms_period
+        self.T = param.num_steps
+        self.fully_connected = param.fully_connected
+        self.method = param.method
+
+        self.num_flawed_robots = num_flawed_robots
+        self.trial_ind = trial_ind
+
+        self.data_dict = {
+            "sim_type": "static_topo_static_degrad_1d",
+            "num_robots": self.num_robots,
+            "num_correct_robots": self.num_correct_robots,
+            "num_flawed_robots": self.num_flawed_robots,
+            "comms_period": self.comms_period,
+            "num_trials": param.num_trials,
+            "num_steps": self.T,
+            "sensor_filter_period": param.sensor_filter_period,
+            "tfr": self.tfr,
+            "flawed_sensor_acc_b": self.init_b,
+            "flawed_sensor_acc_w": self.init_w,
+            "flawed_sensor_cov": self.sensor_cov,
+            "correct_sensor_acc_b": self.act_b,
+            "correct_sensor_acc_w": self.act_w,
+            "fully_connected": self.fully_connected,
+            "method": self.method,
+            "trial_ind": self.trial_ind,
+            "data_str": []
+        }
+
+        self.robot_data_str = [["" for _ in range(self.T+1)] for _ in range(self.num_robots)]
+        self.robot_local_estimates = np.zeros((self.num_robots, self.T+1))
+        self.robot_social_estimates = np.zeros((self.num_robots, self.T+1))
+        self.robot_informed_estimates = np.zeros((self.num_robots, self.T+1))
+        self.robot_sensor_b_estimates = np.zeros((self.num_robots, self.T+1))
+        self.robot_sensor_w_estimates = np.zeros((self.num_robots, self.T+1))
+
+        # Initialize all the robots and their occurrences
+        self.correct_robots = [
+            srsm.Robot(
+                1, # static degradation
+                0.0, # no variance
+                np.array([[self.act_b],[self.act_w]]),
+                np.array([[self.act_b],[self.act_w]]),
+                np.array([[self.sensor_cov],[self.sensor_cov]])
+            ) for _ in range(self.num_correct_robots)
+        ]
+        self.flawed_robots = [
+            srsm.RobotStaticDegradation(
+                param.method,
+                np.array([[self.act_b],[self.act_w]]),
+                np.array([[self.init_b],[self.init_w]]), # assuming that we know what it starts out very well
+                np.array([[self.sensor_cov],[self.sensor_cov]])
+            ) for _ in range(self.num_flawed_robots)
+        ]
+        self.robots = self.correct_robots + self.flawed_robots # flawed robots are appended at the back
+
+        # Simulate observation for all the robots
+        self.occurrences = [self.env.get_occurrences(self.T) for _ in range(self.num_robots)]
+
+        # Initialize random number generator for neighbor selection
+        p = 0.1 # so that we get a right skew
+        self.num_neighbor_selection = stats.binom(self.num_robots, p)
+
+    def run(self):
+
+        # Log initial data
+        self.log_data(0)
+
+        # Run simulation
+        for t in range(1, self.T+1):
+
+            self.step(t)
+
+            # Run sensor filter
+            if t % self.sensor_filter_period == 0:
+                self.sensor_filter_step()
+
+            self.log_data(t)
+
+    def step(self, t):
+
+        # Get the local estimates
+        [robot.compute_local_estimate(occurrence=self.occurrences[ind][t-1]) for ind, robot in enumerate(self.robots)]
+        local_est = [robot.get_local_estimate() for robot in self.robots]
+
+        # Iterate through flawed robots to do social estimation
+        # for ind, robot in enumerate(self.flawed_robots):
+        for ind, robot in enumerate(self.robots):
+
+            # Select a random number of robots to communicate with from both the correct and flawed groups
+            selected_quantity = self.num_robots if self.fully_connected else self.num_neighbor_selection.rvs(size=1)
+
+            x_hat_arr = ()
+            alpha_arr = ()
+
+            if selected_quantity > 0:
+                indices = [i for i in range(self.num_robots) if i != ind] if self.fully_connected else np.random.randint(0, self.num_robots, selected_quantity)
+                x_hat_arr, alpha_arr = zip(*[est_conf_tuple for i, est_conf_tuple in enumerate(local_est) if i != ind and i in indices])
+                robot.compute_social_estimate(x_hat_arr, alpha_arr)
+
+        # Do informed estimation
+        [robot.compute_informed_estimate() for robot in self.robots]
+
+    def sensor_filter_step(self):
+        [self.robots[i].run_sensor_degradation_filter() for i in range(self.num_correct_robots, self.num_robots)]
+
+    def log_data(self, t):
+
+        # Log sensor quality
+        for ind, robot in enumerate(self.robots):
+            est_vals = robot.get_est_sensor_quality()
+            local_est, _ = robot.get_local_estimate()
+            social_est, _ = robot.get_social_estimate()
+            flawed_bool = 1 if ind >= self.num_correct_robots else 0
+
+            data_str = "{0},{1},{2},{3},{4}".format(
+                np.round(local_est, 3),
+                np.round(social_est, 3),
+                np.round(robot.get_informed_estimate(), 3),
+                np.round(est_vals.x[0,0], 3),
+                np.round(est_vals.x[1,0], 3)
+            )
+
+            self.robot_data_str[ind][t] = data_str
+
+    def save_data(self):
+
+        self.data_dict["data_str"] = self.robot_data_str
+
+        path = "flw" + str(self.num_flawed_robots) + "_t" + str(self.trial_ind) + ".json"
+
+        with open(path, "w") as outfile:
+            json.dump(self.data_dict, outfile, indent=4)
